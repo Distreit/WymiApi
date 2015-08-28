9a75310cc6d35e50c7e4e4e4f8c56fcb24dc1eb1
David Streit <davidhstreit@gmail.com>
2015-08-28 02:15:26 +0000

    Removed unused overrides
    git-svn-id: https://98.225.191.7:1443/svn/_Repo/projects/wymi@554 db038745-2a29-7749-a993-e3a7ad6cc4ca

--- trunk/src/main/java/com/hak/wymi/security/RestAuthenticationFailureHandler.java
+++ trunk/src/main/java/com/hak/wymi/security/RestAuthenticationFailureHandler.java
@@ -3,7 +3,6 @@ package com.hak.wymi.security;
 import com.hak.wymi.controllers.rest.helpers.ErrorList;
 import com.hak.wymi.utility.JSONConverter;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 import javax.servlet.ServletException;
@@ -13,14 +12,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class RestAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
-    public RestAuthenticationFailureHandler() {
-        super();
-    }
-
-    public RestAuthenticationFailureHandler(String defaultFailureUrl) {
-        super(defaultFailureUrl);
-    }
-
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
@@ -28,40 +19,5 @@ public class RestAuthenticationFailureHandler extends SimpleUrlAuthenticationFai
         PrintWriter writer = response.getWriter();
         writer.write(JSONConverter.getJSON(new ErrorList(exception.getMessage()), true));
         writer.flush();
-    }
-
-    @Override
-    public void setDefaultFailureUrl(String defaultFailureUrl) {
-        super.setDefaultFailureUrl(defaultFailureUrl);
-    }
-
-    @Override
-    protected boolean isUseForward() {
-        return super.isUseForward();
-    }
-
-    @Override
-    public void setUseForward(boolean forwardToDestination) {
-        super.setUseForward(forwardToDestination);
-    }
-
-    @Override
-    public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
-        super.setRedirectStrategy(redirectStrategy);
-    }
-
-    @Override
-    protected RedirectStrategy getRedirectStrategy() {
-        return super.getRedirectStrategy();
-    }
-
-    @Override
-    protected boolean isAllowSessionCreation() {
-        return super.isAllowSessionCreation();
-    }
-
-    @Override
-    public void setAllowSessionCreation(boolean allowSessionCreation) {
-        super.setAllowSessionCreation(allowSessionCreation);
     }
 }
